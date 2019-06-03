a40ca41afb18613db61275fad50e138006ca381e
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-06-02 23:02:12 -0300

    Adding Cards & Images

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Text('Food Paradise')
+            ],
+          ),
         ),
       ),
     );
